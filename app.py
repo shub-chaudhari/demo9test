19a8e3595f28b8d3b34a9dc576b218ad700457ea
shub-chaudhari <shub.chaudhari@outlook.com>
2025-02-09 13:15:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ def main():
 
     # Input Text Box
     user_input = st.text_input("Enter something:", "")
-
+    
+    pickle_files = ["Model1","Model2","Model3"]
     selected_file = st.selectbox("Select a dictionary file:", pickle_files)
     # Submit Button
     if st.button("Submit"):
         if user_input:
-            reversed_text = user_input[::-1]  # Reverse the text
+            reversed_text = user_input[::-1]+selected_file  # Reverse the text
             st.success(f"Reversed Text: {reversed_text}")
         else:
             st.warning("Please enter some text before submitting.")
