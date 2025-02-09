9deba52bc97cfa2124978de39f2f0baf42d0f592
shub-chaudhari <shub.chaudhari@outlook.com>
2025-02-09 13:52:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,15 @@ def main():
     if st.button("Submit"):
         if user_input:
             reversed_text = user_input[::-1]+selected_file  # Reverse the text
-            st.success(f"summary:\n{reversed_text}")
+            st.success(f"summary:")
+            st.markdown(
+                f"""
+                <div style="border: 2px solid black; padding: 10px; background-color: #f5f5f5; border-radius: 5px;">
+                    {reversed_text}
+                </div>
+                """, 
+                unsafe_allow_html=True
+            )
         else:
             st.warning("Please enter some text before submitting.")
 
